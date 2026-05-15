3b78859885eafa241335aba9d838ceffe3ac0972
Sébastien Cortes <yoyo_seb@hotmail.fr>
2026-05-15 11:46:42 +0200

    Update main.ts

--- cloud-post-api/src/main.ts
+++ cloud-post-api/src/main.ts
@@ -1,8 +1,24 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { NestExpressApplication } from '@nestjs/platform-express'
+import { ConfigService } from '@nestjs/config'
+import { EnvVariables } from './env.validation'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    rawBody: true,
+  })
+
+  app.enableCors()
+  app.set('trust proxy', 'loopback')
+
+  const configService = app.get(ConfigService) as ConfigService<EnvVariables, true>
+  
+  const port = configService.get<EnvVariables['PORT']>('PORT')
+
+  await app.listen(port)
+  
+  console.log(`API listening on port ${port}`)
 }
-bootstrap();
+
+bootstrap()
